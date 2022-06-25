3c97a86842891b59b60ddcee82c8313fbdfff6c6
hem_b <hemanthb93@gmail.com>
2022-06-25 08:20:48 +0530

    [Hemanth]:Subquery is added

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -42,3 +42,38 @@ END;
 INSERT INTO Student_Details VALUES ('jai','IT',19);
 
 select* from Course_Details;
+
+Create table courses (Course_id int not null,Course_name varchar(30))
+
+ALTER TABLE courses ADD CONSTRAINT pk_courseid PRIMARY KEY (Course_id);
+
+alter table Student_Details add Course_id int;
+
+ALTER TABLE Student_Details ADD CONSTRAINT fk_courseid FOREIGN KEY (Course_id) REFERENCES Student_Details(StudentId);
+
+select * from Student_Details;
+
+use StudentDetails;
+select * from courses;
+
+insert into courses values(1,'Data structures'),(2,'Design Principles');
+
+-----------------INNER JOIN, LEFT JOIN, RIGHT JOIN ---------------------
+
+select sd.StudentName,cd.CourseName from Student_Details sd inner join Course_Details cd on sd.StudentId=cd.StudentId;
+
+select sd.StudentName,cd.CourseName from Student_Details sd left join Course_Details cd on sd.StudentId=cd.StudentId;
+
+select sd.StudentName,cd.CourseName from Student_Details sd right join Course_Details cd on sd.StudentId=cd.StudentId;
+
+-----VIEWS-----
+
+CREATE VIEW course_view AS
+SELECT StudentName
+FROM Student_Details;
+
+SELECT * FROM course_view;
+
+--------------------Sub Query --------------------------------
+
+select * from Student_Details where StudentId NOT IN (select StudentId from Course_Details);
